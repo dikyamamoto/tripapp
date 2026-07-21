27be18874dde25975d1a6c0050df66305314a03d
daiki yamamoto <dikyamamoto@users.noreply.github.com>
2026-07-21 17:42:31 +0900

    feat: Nuxt.configの設定(SPA, Supabase)と環境変数サンプルの追加

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  ssr: false, // SPAモード
+  modules: [
+    '@nuxtjs/supabase'
+  ],
+  supabase: {
+    redirectOptions: {
+      login: '/signin',
+      callback: '/confirm',
+      exclude: ['/', '/signup'], // 未ログインでもアクセス可能なパス
+    }
+  }
+})
