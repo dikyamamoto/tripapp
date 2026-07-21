ad272b4f5e2ca65e57471bf4f2f3549ec9ecab1f
daiki yamamoto <dikyamamoto@users.noreply.github.com>
2026-07-21 17:46:16 +0900

    config: 起動ポートを3001に固定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false, // SPAモード
+  devServer: {
+    port: 3001
+  },
   modules: [
     '@nuxtjs/supabase'
   ],
