e8a1a5229cc730ef1d0d45ef1103ffc2cfbdc910
daiki yamamoto <dikyamamoto@users.noreply.github.com>
2026-07-25 20:51:05 +0900

    Fix baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false, // SPAモード
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/tripapp/' : '/'
+  },
   devServer: {
     port: 3001
   },
