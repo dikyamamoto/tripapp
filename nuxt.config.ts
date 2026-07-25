65e5aa6b216f973eeab889cf13ccebd38fb71d5a
daiki yamamoto <dikyamamoto@users.noreply.github.com>
2026-07-25 21:25:37 +0900

    fix: explicit supabase config and deployment workflow for GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,8 @@ export default defineNuxtConfig({
     '@nuxtjs/supabase'
   ],
   supabase: {
+    url: process.env.SUPABASE_URL || 'https://ycbpavascevymilosfdl.supabase.co',
+    key: process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljYnBhdmFzY2V2eW1pbG9zZmRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5Njk2MjUsImV4cCI6MjEwMDU0NTYyNX0.sUokP5Z70zaxbdo4Jwyq76seqaoNniDH_qCb8yo_v5c',
     redirect: false
   }
 })
